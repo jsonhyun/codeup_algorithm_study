c24d95e26cbef9a514c9c53ea6dd4581754fd1e0
jsonhyun <airplant02342@hotmail.com>
2020-01-06 21:24:17 +0900

    코드업 기초100제 문제 8번

---
+++ src/codeup_algorithm_study/basic100/No1008_기초_출력_출력하기_08.java
@@ -0,0 +1,9 @@
+package codeup_algorithm_study.basic100;
+
+public class No1008_기초_출력_출력하기_08 {
+
+	public static void main(String[] args) {
+		System.out.printf("\u250c\u252c\u2510%n\u251c\u253c\u2524%n\u2514\u2534\u2518");
+	}
+
+}
