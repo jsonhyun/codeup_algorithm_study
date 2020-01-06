c61c1af5bcf9ca0d0464e06becdb8876283f3095
jsonhyun <airplant02342@hotmail.com>
2020-01-06 21:07:23 +0900

    코드업 기초100제 문제 7번

---
+++ src/codeup_algorithm_study/basic100/No1007_기초_출력_출력하기_07.java
@@ -0,0 +1,10 @@
+package codeup_algorithm_study.basic100;
+
+public class No1007_기초_출력_출력하기_07 {
+
+	public static void main(String[] args) {
+		System.out.println("\"C:\\Download\\hello.cpp\"");
+
+	}
+
+}
