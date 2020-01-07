9f7e05a4983c1cb80abd63e812722fa9390be974
jsonhyun <airplant@naver.com>
2020-01-07 20:14:07 +0900

    코드업 기초100제 문제 12번

---
+++ src/codeup_algorithm_study/basic100/No1012_기초_입출력_실수1개_입력받아_그대로_출력하기.java
@@ -0,0 +1,14 @@
+package codeup_algorithm_study.basic100;
+
+import java.util.Scanner;
+
+public class No1012_기초_입출력_실수1개_입력받아_그대로_출력하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float a;
+		a = sc.nextFloat();
+		System.out.printf("%f",a);
+		sc.close();
+	}
+}
