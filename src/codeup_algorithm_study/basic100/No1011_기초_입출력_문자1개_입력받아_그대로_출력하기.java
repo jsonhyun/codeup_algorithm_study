7cec7089bceb0524c4a8d2133264e45ddaf4dd53
jsonhyun <airplant@naver.com>
2020-01-07 20:13:59 +0900

    코드업 기초100제 문제 11번

---
+++ src/codeup_algorithm_study/basic100/No1011_기초_입출력_문자1개_입력받아_그대로_출력하기.java
@@ -0,0 +1,14 @@
+package codeup_algorithm_study.basic100;
+
+import java.util.Scanner;
+
+public class No1011_기초_입출력_문자1개_입력받아_그대로_출력하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a;
+		a = sc.next();
+		System.out.println(a);
+		sc.close();
+	}
+}
