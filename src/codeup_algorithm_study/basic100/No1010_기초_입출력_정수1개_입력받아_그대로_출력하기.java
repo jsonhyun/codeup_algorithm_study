90939e1b5a312d2b8c2441eb0f06da90da0715ed
jsonhyun <airplant@naver.com>
2020-01-07 20:13:41 +0900

    코드업 기초100제 문제 10번

---
+++ src/codeup_algorithm_study/basic100/No1010_기초_입출력_정수1개_입력받아_그대로_출력하기.java
@@ -0,0 +1,14 @@
+package codeup_algorithm_study.basic100;
+
+import java.util.Scanner;
+
+public class No1010_기초_입출력_정수1개_입력받아_그대로_출력하기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a = sc.nextInt();
+		System.out.println(a);
+		sc.close();
+	}
+}
